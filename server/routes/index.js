5ea8a35fd35bc044c6c259d323345be48eb3d760
Teazer0 <jasmine24611755@gmail.com>
2021-01-10 02:00:39 +0800

    Teacher.ejs and AmoebaPosition.ejs finish

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,5 +8,119 @@ router.get("/", function (req, res, next) {
 router.get("/test", function (req, res, next) {
   res.render("index", { title: "Test123" });
 });
+router.get("/teacher", function (req, res, next) {
+  res.render("Teacher", {
+    date:"2021/1/12",
+    question: [
+      "I don't know how to program JS",
+      " count down 6 days to winter vocation"
+     ],
+    students: [
+      {
+        StuID: "A1075500",
+        Temp: "70",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "3",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+    ]
+  });
+});
+
+router.get("/AmoebaPosition", function (req, res, next) {
+  res.render("AmoebaPosition", {
+    date:"2021/1/12",
+    courseID: "CSC111",
+    courseName: "嵌入式系統",
+    courseTeacher: "郭錦福",
+    courseDate: "2021/11/87",
+    courseClassroom: "工院200",
+    students: [
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "40",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "3",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+      {
+        StuID: "A1075500",
+        Temp: "37",
+        Time: "13:00"
+      },
+    ]
+  });
+});
 
 module.exports = router;
