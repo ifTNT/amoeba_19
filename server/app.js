43acf23a0905a418ec6089a1b10d181c97eca2ef
addG-Kao <a1075521@mail.nuk.edu.tw>
2021-01-10 04:18:27 +0800

    Student's all interface

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var stylus = require("stylus");
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -16,8 +18,13 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'This is a secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,8 +1,32 @@
 var express = require("express");
 var db = require("./db");
+var crypto = require('crypto')
 var router = express.Router();
 
 /* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("StudentHome");
+});
+/* POST home page. */
+router.post("/", function (req, res, next) {
+  console.log(req.body);
+  // Hash the password
+  let shasum = crypto.createHash('sha1');
+  shasum.update(req.body.password);
+  let hash_passwd = shasum.digest('hex');
+  let user = req.body.studentID;
+
+  // 和資料庫的密碼比對
+  db.get("SELECT Password FROM `Accounts` WHERE StuID = ?", [user], (error, passwd)=>{
+    if(passwd['Password']!==hash_passwd){
+      res.redirect("/student/");
+    }
+    else{
+      req.session['user'] = user;
+      res.redirect("/student/temperature");
+    }
+  });
+});
 router.get("/slides", function (req, res, next) {
   //從DB抓資料
   db.all("SELECT Content FROM `Questions`", [], (error, rows)=>{
@@ -15,8 +39,52 @@ router.get("/slides", function (req, res, next) {
     });
   });
 });
-router.get("/test", function (req, res, next) {
-  res.render("index", { title: "Test123" });
+router.get("/temperature", function (req, res, next){
+  let user = req.session['user'];
+  db.get("SELECT `Name` FROM `Accounts` WHERE `StuID` = ?",[user],(error, rows)=>{
+    res.render("StudentTemp",{
+      name: rows['Name'],
+      studentid: user
+    });
+  });
+});
+router.get("/tempok", function (req, res, next){
+  let user = req.session['user'];
+  let tempok = "37.5";
+  db.get("SELECT `Name` FROM `Accounts` WHERE `StuID` = ?",[user],(error,name)=>{
+    res.render("StudentInfo_ok",{
+      name: name['Name'],
+      studentid: user,
+      temperature: tempok
+    });
+  });
+});
+router.get("/tempwarning", function (req, res, next){
+  let user = req.session['user'];
+  let tempwarning = "38.2";
+  db.get("SELECT `Name` FROM `Accounts` WHERE `StuID` = ?",[user],(error,name)=>{
+    res.render("StudentInfo_warning",{
+      name: name['Name'],
+      studentid: user,
+      temperature: tempwarning
+    });
+  });
+});
+router.get("/tempdanger", function (req, res, next){
+  let user = req.session['user'];
+  let tempdanger = "38.3";
+  db.get("SELECT `Name` FROM `Accounts` WHERE `StuID` = ?",[user],(error,name)=>{
+    res.render("StudentInfo_danger",{
+      name: name['Name'],
+      studentid: user,
+      temperature: tempdanger
+    });
+  });
+});
+
+router.get("/logout", function (req, res, next) {
+  delete req.session['user'];
+  res.redirect("/student/");
 });
 
 module.exports = router;
